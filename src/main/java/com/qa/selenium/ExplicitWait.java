242d7d22d15126476cc377d1de4e5e51f9a12e0f
abhijithspishere <abhijithspishere@gmail.com>
2024-09-18 15:36:45 +0530

    Added Explicit Wait

---
+++ src/main/java/com/qa/selenium/ExplicitWait.java
@@ -0,0 +1,48 @@
+package com.qa.selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExplicitWait {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement userName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Username']")));
+        userName.sendKeys("Admin");
+
+        WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Password']")));
+        password.sendKeys("admin123");
+
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Login']")));
+        submitButton.click();
+
+        WebElement loginSuccess = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h6[normalize-space()='Dashboard']")));
+
+        if (loginSuccess.getText().equals("Dashboard")) {
+            System.out.println("Login success");
+            
+        } else {
+            System.out.println("Login Failure");
+        }
+
+        driver.quit();
+
+
+    }
+}
