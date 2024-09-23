887982cf771abbf13eb9270a1292ce5bfbd5ea11
abhijithspishere <abhijithspishere@gmail.com>
2024-09-23 16:36:43 +0530

    FirstAssignment_Amazon_Added

--- src/main/java/com/qa/selenium/TaskAmazon.java
+++ src/main/java/com/qa/selenium/TaskAmazon.java
@@ -1,16 +1,19 @@
 package com.qa.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TaskAmazon {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
@@ -19,20 +22,48 @@ public class TaskAmazon {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         WebElement menuBar = 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//i[@class='hm-icon nav-sprite']")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//i[@class='hm-icon nav-sprite']")));
         menuBar.click();
+        // System.out.println("click1");
+
+        // Thread.sleep(10);
 
         WebElement menuComputer = 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[normalize-space()='Mobiles, Computers']")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[normalize-space()='Mobiles, Computers']")));
         menuComputer.click();
-        System.out.println("click1");
+        // System.out.println("click2");
+       
+        
+        /* WebElement menuLaptop =
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Laptops')]")));
+        menuLaptop.click(); */
 
-        WebElement menuLaptop =
+        WebElement menuLaptop = 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Laptops')]")));
-        menuLaptop.click();
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", menuLaptop);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", menuLaptop);
 
+        
         WebElement checkBoxApple = 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@for='apb-browse-refinements-checkbox_7']//i[@class='a-icon a-icon-checkbox']")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='apb-browse-refinements-checkbox_7']//i[@class='a-icon a-icon-checkbox']")));
         checkBoxApple.click();
+
+        List<WebElement> allButton = driver.findElements(By.tagName("button"));
+
+        int addToCartCount = 0;
+        
+        for (WebElement eachButton : allButton) {
+            String buttonLink = eachButton.getText().trim();
+            // System.out.println(buttonLink);
+
+            if (buttonLink.equalsIgnoreCase("Add to Cart")) {
+                addToCartCount++;
+            //    System.out.println(addToCartCount);
+            }
+            if (addToCartCount==1) {
+                ((JavascriptExecutor)driver).executeScript("arguments[0].click();", eachButton);
+                break;
+            }
+        }
     }
 }
